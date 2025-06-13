82cd99cf503cf7a73577d07d4173fd0d4701cdae
akshayagarwal2912 <122619918+akshayagarwal2912@users.noreply.github.com>
2025-06-13 08:50:15 +0530

    Delete Day2_PracticeProblems directory

--- Day2_PracticeProblems/Level 1/PenDistribution.java
+++
@@ -1,9 +0,0 @@
-public class PenDistribution {
-    public static void main(String[] args) {
-        int totalPens = 14;// Example total number of pens
-        int students = 3;// Example number of students
-        int pensPerStudent = totalPens / students;// Calculate pens per student by dividing total pens by number of students
-        int remainingPens = totalPens % students;// Calculate remaining pens by using modulus operator
-        System.out.println("The Pen Per Student is " + pensPerStudent + " and the remaining pen not distributed is " + remainingPens);
-    }
-}
