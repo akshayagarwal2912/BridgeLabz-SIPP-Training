18aa019acc4bfbbb5d3d04875e5dad77e2dd707a
akshayagarwal2912 <122619918+akshayagarwal2912@users.noreply.github.com>
2025-06-13 08:49:47 +0530

    Delete Day1_ProgrammingElements directory

--- Day1_ProgrammingElements/Welcome1.java
+++
@@ -1,8 +0,0 @@
-public class Welcome1 {
-    
-    public static void main(String[] args) {
-        System.out.println("Welcome to Bridgelabz!"); 
-        System.out.println("Welcome to Bridgelabz!");  
-    }
-    
-}
