b7150e2d418b30ce36cb8314b5ab912801800b82
akshayagarwal2912 <officialakshayagarwal@gmail.com>
2025-06-15 19:52:17 +0530

    "Added: Program to Check Largest among 3 Numbers"

---
+++ Control_Flow/LargestNumberCheck.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class LargestNumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking 3 numbers from the user
+        System.out.print("Enter first number: ");
+        int number1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int number2 = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int number3 = sc.nextInt();
+
+        // Check if first number is the largest
+        if (number1 > number2 && number1 > number3) {
+            System.out.println("Is the first number the largest? Yes");
+        } else {
+            System.out.println("Is the first number the largest? No");
+        }
+
+        // Check if second number is the largest
+        if (number2 > number1 && number2 > number3) {
+            System.out.println("Is the second number the largest? Yes");
+        } else {
+            System.out.println("Is the second number the largest? No");
+        }
+
+        // Check if third number is the largest
+        if (number3 > number1 && number3 > number2) {
+            System.out.println("Is the third number the largest? Yes");
+        } else {
+            System.out.println("Is the third number the largest? No");
+        }
+    }
+}
