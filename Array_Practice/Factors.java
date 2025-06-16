ad08a192a5b60138bf7b731f9653bc036ec04599
akshayagarwal2912 <officialakshayagarwal@gmail.com>
2025-06-16 11:47:59 +0530

    "Added: Program to find Factors"

--- Array_Practice/Factors.java
+++ Array_Practice/Factors.java
@@ -1,29 +1,15 @@
 import java.util.*;
 
-public class EvenOrOdd {
+public class Factors {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("Enter the number of elements to be checked:");
+        System.out.println("Enter the number to find its factors:");
         int n = scn.nextInt();
 
-        int[] arr = new int[n];
-        int[] evenArr = new int[n];
-        int[] oddArr = new int[n];
-
-        // Take input for the array
-        System.out.println("Enter " + n + " numbers:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scn.nextInt();
-        }
-
-        // Separate even and odd elements
-        for (int i = 0; i < n; i++) {
-            if (arr[i] % 2 == 0) {
-                evenArr[i] = arr[i];
-                System.out.println("Even element at index " + i + " is " + evenArr[i]);
-            } else {
-                oddArr[i] = arr[i];
-                System.out.println("Odd element at index " + i + " is " + oddArr[i]);
+        System.out.println("Factors of " + n + " are:");
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.println(i); // Print only if it's a factor
             }
         }
     }
