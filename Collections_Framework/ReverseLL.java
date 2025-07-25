0a00ff6f797142cbad2e8b674d6c7260e9d79334
akshayagarwal2912 <officialakshayagarwal@gmail.com>
2025-07-25 09:54:37 +0530

    "Added: Program to Reverse a Linked List."

--- Collections_Framework/ReverseLL.java
+++ Collections_Framework/ReverseLL.java
@@ -1,7 +1,29 @@
 import java.util.*;
 public class ReverseLL {
     public static void main(String[] args){
-        LinkedList<Integer> list = new LinkedList<>();
+        
+        Scanner scn = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        
+        System.out.println("Enter the number of elements");
+        int n = scn.nextInt();
+        
+        for(int i = 0; i < n; i++){
+            System.out.println("Enter the " + (i+1) +" element ");
+            int value = scn.nextInt();
+            list.add(value);
+        }
+        
+        System.out.println("List before Reverse: " + list);
+        
+        List<Integer> newList = new ArrayList<>();
+        
+        for(int i = n - 1; i >= 0; i--){
+        int val = list.get(i);
+            newList.add(val);
+        }
+        
+        System.out.println("List after Reverse: " + newList);
         
     }
 }
